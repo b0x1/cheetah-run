13d721721947387d8d17f70ebc63a3e0b35fd27e
Boyang Xia <boyang.xia@gepardec.com>
2017-07-20 16:19:41 +0200

    Move background when cheetah runs

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,16 +1,22 @@
+var CONSTANT_RUN = 400;
+
+
 Cheetah = function(name, imageSource) {
   this.name = name;
   this.image = new Image();
   this.image.src = imageSource;
   this.stepSize = 10;
   this.posX = 10;
-  this.posY = 155;
+  this.posY = 800;
+  this.distance = this.posX;
+  this.lastkey;
 
   this.run = function(evt) {
-    if (this.posX + this.stepSize < canvas.width) {
+    if (this.posX + this.stepSize < CONSTANT_RUN) {
       this.posX += this.stepSize;
-      canvas.lastkey = evt.key;
     }
+    this.distance += this.stepSize;
+    this.lastkey = evt.key;
   }
 
   window.onkeypress = this.run.bind(this);
@@ -28,17 +34,19 @@ Canvas = function(canvasElement, bgImage, cheetah) {
   this.bubble = function() {
     this.ctx.font = "30pt Arial";
     this.ctx.fillStyle = "#f7f7f4";
-    if (this.lastkey)
-      this.ctx.fillText(this.lastkey, this.cheetah.posX + 30, this.cheetah.posY - 30);
-  }
-
-  this.clear = function() {
-    this.ctx.clearRect(0, 0, this.width, this.height);
+    if (cheetah.lastkey)
+      this.ctx.fillText(cheetah.lastkey, this.cheetah.posX + 30, this.cheetah.posY - 30);
   }
 
   this.draw = function() {
-    this.clear();
-    this.ctx.drawImage(this.background, 0, 0);
+    this.ctx.clearRect(0, 0, this.width, this.height); // Clear Canvas
+
+    if (cheetah.distance < CONSTANT_RUN) {
+      this.ctx.drawImage(this.background, 0, 0);
+    } else {
+      this.ctx.drawImage(this.background, cheetah.distance, 0, this.width, this.height, 0, 0, this.width, this.height);
+    }
+
     this.bubble();
     this.ctx.drawImage(this.cheetah.image, this.cheetah.posX, this.cheetah.posY);
   }
@@ -49,4 +57,4 @@ Canvas = function(canvasElement, bgImage, cheetah) {
 
 
 cheetah = new Cheetah("Gepardec", "images/cheetah.png");
-canvas = new Canvas("cheetah-track", "images/background.jpeg", cheetah);
+canvas = new Canvas("cheetah-track", "images/steppe.png", cheetah);
