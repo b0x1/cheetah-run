6e02bbf158461be26ea46f43288c7d97b59aabe6
Boyang Xia <boyang.xia@gepardec.com>
2017-07-20 11:44:35 +0200

    Prototypify JS objects

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,51 +1,52 @@
+Cheetah = function(name, imageSource) {
+  this.name = name;
+  this.image = new Image();
+  this.image.src = imageSource;
+  this.stepSize = 10;
+  this.posX = 10;
+  this.posY = 155;
 
-var cheetah = {
-  name: "Gepardec",
-  image: new Image(),
-  stepSize: 10,
-  posX: 10,
-  posY: 155,
-  init: function() {
-    this.image.src = "images/cheetah.png";
-    window.onkeypress = this.run.bind(this);
-  },
-  run: function(evt) {
+  this.run = function(evt) {
     if (this.posX + this.stepSize < canvas.width) {
-      this.posX += this.stepSize; 
+      this.posX += this.stepSize;
       canvas.lastkey = evt.key;
     }
   }
-};
 
-var canvas = {
-  background: new Image(),
-  init: function() {
-    window.focus();
-    this.background.src = "images/background.jpeg";
-    this.elem = document.getElementById("cheetah-track");
-    this.ctx = this.elem.getContext("2d");
-    this.width = this.elem.width;
-    this.height = this.elem.height;
+  window.onkeypress = this.run.bind(this);
+}
 
-    return setInterval(this.draw.bind(this), 30);
-  },
-  bubble: function() {
+Canvas = function(canvasElement, bgImage, cheetah) {
+  this.background = new Image();
+  this.background.src = bgImage;
+  this.elem = document.getElementById(canvasElement);
+  this.ctx = this.elem.getContext("2d");
+  this.width = this.elem.width;
+  this.height = this.elem.height;
+  this.cheetah = cheetah;
+
+  this.bubble = function() {
     this.ctx.font = "30pt Arial";
     this.ctx.fillStyle = "#f7f7f4";
     if (this.lastkey)
-      this.ctx.fillText(this.lastkey, cheetah.posX + 30, cheetah.posY - 30);  
-  },
-  clear: function() {
+      this.ctx.fillText(this.lastkey, this.cheetah.posX + 30, this.cheetah.posY - 30);
+  }
+
+  this.clear = function() {
     this.ctx.clearRect(0, 0, this.width, this.height);
-  },
-  draw: function() {
+  }
+
+  this.draw = function() {
     this.clear();
-    this.ctx.drawImage(canvas.background, 0, 0);
+    this.ctx.drawImage(this.background, 0, 0);
     this.bubble();
-    this.ctx.drawImage(cheetah.image,cheetah.posX,cheetah.posY);
-  },
-};
+    this.ctx.drawImage(this.cheetah.image, this.cheetah.posX, this.cheetah.posY);
+  }
+
+  window.focus();
+  setInterval(this.draw.bind(this), 30);
+}
 
 
-cheetah.init();
-canvas.init();
+cheetah = new Cheetah("Gepardec", "images/cheetah.png");
+canvas = new Canvas("cheetah-track", "images/background.jpeg", cheetah);
