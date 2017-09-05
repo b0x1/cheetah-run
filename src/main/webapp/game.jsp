7dde19e948735092e24b8cfcf37b746f4e707fe9
Maurice el-Banna <mbanna@localhost.localdomain>
2017-09-05 11:48:17 +0200

    Adding Timeline + Adding WinScreen + Fixing a bug where the cheetah kisses the wall on the end + Fixing Balloon positioning

--- src/main/webapp/game.jsp
+++ src/main/webapp/game.jsp
@@ -5,9 +5,32 @@
 <jsp:body>
 
   <section>
+    <div id="win-screen" style="visibility: hidden;">
+        <img src="images/imgYouWon.jpg" style="visibility: hidden;" />
+    </div>
     <canvas id="cheetah-track">
       Glory to HTML5 Canvas!
     </canvas>
+    <div id="timeline-container">
+        <div id="timeline">
+        </div>
+    </div>
+    <style>
+      #win-screen{
+
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+
+        text-alignment: center;
+        vertical-alignment: middle;
+        font-size: 50px;
+
+        background-image: url("images/imgYouWon.jpg");
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+      }
+    </style>
     <script type="text/javascript">
     var imagePrefix = "images/";
     </script>
@@ -17,6 +40,7 @@
     <script type="text/javascript" src="js/canvasobjects.js"></script>
     <script type="text/javascript" src="js/game.js"></script>
   </section>
-
+  <%@ taglib uri="http://java.sun.com/jsf/html" prefix="h" %>
+  
 </jsp:body>
 </t:base>
--- src/main/webapp/js/game.js
+++ src/main/webapp/js/game.js
@@ -28,16 +28,25 @@ for (var i = 0; i < 17; i++) {
 }
 
 var pictureData = [
-  ["cateringBalloon.png", 10, 10],
-  ["demoBalloon.png", 100, 20],
-  ["iPadBalloon.png", 300, 30],
-  ["networkingBalloon.png", 1000, 40],
-  ["skystageBalloon.png", 1500, 50],
-  ["speakerBalloon.png", 2300, 60]
+  ["cateringBalloon.png", 1, 10],
+  ["demoBalloon.png", 2, 10],
+  ["cateringBalloon.png", 3, 10],
+  ["demoBalloon.png", 4, 10],
+  ["cateringBalloon.png", 5, 10],
+  ["demoBalloon.png", 6, 10],
+  ["cateringBalloon.png", 7, 10],
+  ["demoBalloon.png", 8, 10],
+  ["cateringBalloon.png", 9, 10],
+  ["iPadBalloon.png", 10, 30],
+  ["networkingBalloon.png", 32, 40],
+  ["skystageBalloon.png", 64, 50],
+  ["speakerBalloon.png", 40, 60]
 ];
 var pictures = [];
 for (var i = 0; i < pictureData.length; i++) {
   pictureData[i][0] = getImage(imagePrefix + pictureData[i][0]);
+
+  pictureData[i][1] = (pictureData[i][1]);
 }
 
 
@@ -52,6 +61,8 @@ window.onload = function() {
   }
   canvas = new AugmentedCanvas("cheetah-track", canvasBgImage, cheetah, pictures);
 
+  drawTimeline();
+
   canvas.draw();
 //  clickAction();
 }
@@ -64,6 +75,63 @@ window.onresize = function()  {
     pictures[i].resize();
   }
   canvas.draw();
+}
+
+
+// Function to draw a configurable timeline below the game (it draws only one time and gets moved in canvas.js in AugementedCanvas)
+function drawTimeline(){
+  /*
+  timelineBigNumSteps: How often big numbers should be printed in the timeline (for example "=32" means every 32 steps)
+  timelineSmallNumSteps: It says "num" because it was planned as a small num. Its actually a vertical line
+  timelineStart: With which number the timeline starts
+  timelineStop: might be obvious...
+  timelineStartAndStopAlsoBig: If start and stop do not fit in the requirements for timelineBigNumSteps, but you still want to print them big, make it "true"
+  timelineStepSize: the width of the elements
+  timelineElementString: The html string with the timeline
+  */
+  var timelineBigNumSteps=32, timelineSmallNumSteps=8, timelineStart=0, timelineStop=PARAMETERS.maximum_steps, timelineStartAndStopAlsoBig=true, timelineStepSize=canvas.stepSize
+  var timelineElementString=""
+  for(var i=timelineStart; i<=timelineStop; i++){
+    // Start and Stop printed big, if enabled
+    if((i==timelineStart || i==timelineStop) && timelineStartAndStopAlsoBig){
+      timelineElementString+=   "<div class='timeLineBigNumContainer' style='float: left; width: "+(timelineStepSize)+"px;'>"
+        +                           "<div class='timeLineVeryBigNumNumber' style='font-size: 30px; font-weight: bold; text-align: center;'>"
+        +                           ""+i
+        +                           "</div>"
+        +                       "</div>"
+    // Big numbers
+    }else if(i%timelineBigNumSteps==0){
+      timelineElementString+=   "<div class='timeLineBigNumContainer' style='float: left; width: "+(timelineStepSize)+"px; text-align: center;'>"
+        +                           "<div class='timeLineBigNumNumber' style='text-align: center; width: "+(timelineStepSize)+"px; font-size: 18px; font-weight: bold; margin-top: 7px'>"
+        +                           ""+i
+        +                           "</div>"
+        +                       "</div>"
+    // Small "numbers"
+    }else if(i%timelineSmallNumSteps==0){
+      timelineElementString+=   "<div class='timeLineSmallNumContainer' style='float: left; width: "+timelineStepSize+"px'>"
+        +                           "<div class='timeLineSmallNumBarContainer' style='width: "+(timelineStepSize)+"px;'>"
+        +                           "<div class='timeLineSmallNumBar' style='width: "+(timelineStepSize/2-2)+"px; height: 30px; background-color: none; margin-top: 5px; float: left;'>"
+        +                           "</div>"
+        +                           "<div class='timeLineSmallNumBar' style='width: 4px; height: 30px; background-color: black; margin-top: 5px;float: left;'>"
+        +                           "</div>"
+        +                           "</div>"
+        +                       "</div>"
+    // If there is nothing to print, it prints a vertical line
+    }else{
+      timelineElementString+=   "<div style='height: 40px; width: 40px; float: left'><div style='float: left; height: 4px; width: "+(timelineStepSize)+"px; background-color: black; margin-top: "+(timelineStepSize/2-2)+"px;' > </div></div>"
+    }
+  }
+  // Setting up the style of the timeline & its container + filling the html in it
+  var timeline=document.getElementById('timeline')
+  var timelineContainer=document.getElementById('timeline-container')
+  timeline.innerHTML=timelineElementString
+  timeline.style.width="auto"
+  timeline.style.height="40px"
+  timelineContainer.style.width="100%"
+  timelineContainer.style.height="40px"
+  timelineContainer.style.overflow="hidden"
+
+  // -> Movement of the timeline is in canvas.js
 }
 
 function clickAction() {
@@ -80,11 +148,19 @@ function clickAction() {
       canvas.textBubbles.push(new TextBubble(canvas.ctx, "Spiel zu Ende!",
                                       PARAMETERS.canvas.width/3, PARAMETERS.canvas.height/4 , true));
       canvas.draw();
+      showWinScreen();
     } else {
       setTimeout(clickAction, 80);
     }
 
   });
+}
+
+// When the game is finished (Cheetah reaches the goal), a winscreen is shown
+function showWinScreen(){
+  var winScreen=document.getElementById("win-screen")
+
+  winScreen.style.visibility="visible"
 }
 
 // Test code
@@ -93,12 +169,13 @@ function clickAction() {
    canvas.textBubbles.push(new TextBubble(canvas.ctx, evt.key + "(" +  (processedClick+1) + ")", cheetah.posX + 30, cheetah.posY - 30));
    processedClick += 1;
    if (processedClick >= PARAMETERS.maximum_steps) {
+     canvas.draw();
+     showWinScreen();
      stopGame();
    } else {
      canvas.draw();
    }
  };
-// End test code
 
 window.focus();
 
@@ -106,6 +183,7 @@ var stopGame = function() {
   window.onkeypress = undefined;
   clearInterval(canvas.animate);
 }
+// End test code
 
 
 
